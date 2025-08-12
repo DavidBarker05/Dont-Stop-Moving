fc7c4b18703af7a0dae6a92ee72ba6b64de3944e
DavidBarker05 <164867676+DavidBarker05@users.noreply.github.com>
2025-08-12 18:42:02 +0200

    Finalised cop manager logic hopefully
    Should just need to add actual values to things and not placeholders

--- Assets/Scripts/Cops/CopManager.cs
+++ Assets/Scripts/Cops/CopManager.cs
@@ -21,11 +21,11 @@ public class CopManager : MonoBehaviour
     [SerializeField]
     [Tooltip("How long until the manager spawns cops")]
     [Min(0)]
-    float spawnTime;
+    float spawnDelay;
     [SerializeField]
-    [Tooltip("How long cops will pursue the player for")]
+    [Tooltip("How long cops until the cops are despawned after being spawned")]
     [Min(0)]
-    float pursuitTime;
+    float despawnDelay;
 
     System.Collections.Generic.List<CopAgent> agents = new System.Collections.Generic.List<CopAgent>();
 
@@ -37,42 +37,64 @@ public class CopManager : MonoBehaviour
         else Instance = this;
     }
 
-    void Start()
-    {
-    }
+    void Start() => StartCoroutine(SpawnCops(delay: spawnDelay));
 
-    void Update()
+    IEnumerator SpawnCops(float delay)
     {
-    }
-
-    IEnumerator SpawnCops()
-    {
-        yield return new WaitForSeconds(spawnTime);
-        if (agents.Count > 0) DespawnCops();
+        yield return new WaitForSeconds(delay);
+        if (agents.Count > 0) StartCoroutine(DespawnCops(delay: 0f));
         for (int i = 0; i < Random.Range(minCops, maxCops); i++) agents.Add(Instantiate(copPrefab));
-        DespawnCops();
+        attackingAgent = agents[Random.Range(0, agents.Count)];
+        StartCoroutine(ChangeAgentState(delay: 0f, offset: Vector3.zero, newCopState: CopAgent.CopState.Engaging)); // Do an actual delay and offset at some point
+        StartCoroutine(DespawnCops(delay: despawnDelay));
     }
 
-    IEnumerator DespawnCops()
+    IEnumerator DespawnCops(float delay)
     {
-        yield return new WaitForSeconds(pursuitTime);
+        yield return new WaitForSeconds(delay);
         if (agents.Count <= 0) yield break;
         foreach (CopAgent agent in agents) Destroy(agent.gameObject);
         agents.Clear();
-        SpawnCops();
+        SpawnCops(delay: spawnDelay);
+    }
+
+    IEnumerator ChangeAgentState(float delay, Vector3 offset, CopAgent.CopState newCopState)
+    {
+        yield return new WaitForSeconds(delay);
+        if (agents.Count <= 0 || attackingAgent == null) yield break;
+        attackingAgent.Offset = offset;
+        attackingAgent.CurrentCopState = newCopState;
     }
 
     public void MoveAgentToNextState()
     {
-        switch (attackingAgent.CurrentCopState)
+        float delay = attackingAgent.CurrentCopState {
+            CopAgent.CopState.Engaging => 0f, // Do an actual delay at some point
+            CopAgent.CopState.Preparing => 0f, // Do an actual delay at some point
+            CopAgent.CopState.Attacking => 0f, // Do an actual delay at some point
+            CopAgent.CopState.Disengaging => 0f, // Do an actual delay at some point
+            _ => 0f // Fallback value, should in theory never be needed
+        };
+        Vector3 offset = attackingAgent.CurrentCopState {
+            CopAgent.CopState.Engaging => Vector3.zero, // Do an actual offset at some point
+            CopAgent.CopState.Preparing => Vector3.zero, // Do an actual offset at some point
+            CopAgent.CopState.Attacking => Vector3.zero, // Do an actual offset at some point
+            CopAgent.CopState.Disengaging => Vector3.zero, // Do an actual offset at some point
+            _ => Vector3.zero // Fallback value, should in theory never be needed
+        };
+        CopAgent.CopState newCopState = attackingAgent.CurrentCopState {
+            CopAgent.CopState.Engaging => CopAgent.CopState.Preparing,
+            CopAgent.CopState.Preparing => CopAgent.CopState.Attacking,
+            CopAgent.CopState.Attacking => CopAgent.CopState.Disengaging
+            CopAgent.CopState.Disengaging => CopAgent.CopState.Engaging
+            _ => Vector3.Driving // Fallback value, should in theory never be needed
+        };
+        if (attackingAgent.CurrentCopState == CopAgent.CopState.Disengaging)
         {
-            case (CopAgent.CopState.Disengaging):
-                attackingAgent.Offset = Vector3.zero; // Do an actual offset at some point
-                attackingAgent.CurrentCopState = CopAgent.CopState.Driving;
-                attackingAgent = agents[Random.Range(0, agents.Count)];
-                attackingAgent.Offset = Vector3.zero; // Do an actual offset at some point
-                attackingAgent.CurrentCopState = CopAgent.CopState.Engaging;
-                break;
+            attackingAgent.Offset = Vector3.zero; // Do an actual offset at some point
+            attackingAgent.CurrentCopState = CopAgent.CopState.Driving;
+            attackingAgent = agents[Random.Range(0, agents.Count)];
         }
+        StartCoroutine(ChangeAgentState(delay: delay, offset: offset, newCopState: newCopState));
     }
 }
